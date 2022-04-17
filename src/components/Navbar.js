a8e978de6b7987cf4698786bf8c1c5cf4552e73b
Springhawk27 <sajjad0mahmud@gmail.com>
2022-04-17 16:55:44 +0600

    variables created and applied

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,12 @@ import '../Styles/Navbar.css'
 function Navbar() {
     return (
         <div className='navbar'>
-            <a href="/home">Home</a>
-            <a href="/about">About</a>
-            <a href="/profile">Profile</a>
-            <a href="/contact">Contact</a>
+            <div className='links'>
+                <a href="/home">Home</a>
+                <a href="/about">About</a>
+                <a href="/profile">Profile</a>
+                <a href="/contact">Contact</a>
+            </div>
         </div>
     )
 }
