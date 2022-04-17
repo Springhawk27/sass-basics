33a5fb645aef1cc503d968d63afd7c4c31af5ea5
Springhawk27 <sajjad0mahmud@gmail.com>
2022-04-17 19:14:57 +0600

    tried nesting mixin import and functions

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../Styles/Home.css'
+
+function Home() {
+    return (
+        <div className='home'>
+            <h1> Welcome to Home</h1>
+            <button>Submit</button>
+        </div>
+    )
+}
+
+export default Home
